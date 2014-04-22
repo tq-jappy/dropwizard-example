b793da353f6051c92e02a05a860def5ad6681dea
Takumi Endo <t_endo@tq-mbp.local>
2014-04-22 20:49:46 +0900

    log

--- src/main/java/example/websocket/echo/EchoWebSocket.java
+++ src/main/java/example/websocket/echo/EchoWebSocket.java
@@ -11,22 +11,23 @@ import org.slf4j.LoggerFactory;
 @WebSocket
 public class EchoWebSocket {
 
-    private Logger logger = LoggerFactory.getLogger(EchoWebSocket.class);
+    private static final Logger log = LoggerFactory
+            .getLogger(EchoWebSocket.class);
 
     @OnWebSocketConnect
     public void onConnect(Session session) {
-        logger.debug("client connected: %s", session);
+        log.debug("client connected: {}", session.getLocalAddress());
     }
 
     @OnWebSocketMessage
     public void onText(Session session, String message) {
-        logger.debug("message received: %s", session);
+        log.debug("message received: {}", session);
         session.getRemote().sendStringByFuture(message);
     }
 
     @OnWebSocketClose
     public void close(Session session, int statusCode, String reason) {
-        logger.debug("client closed: %s (code: %d, reason: %s)", session,
+        log.debug("client closed: {} (code: {}, reason: {})", session,
                 statusCode, reason);
     }
 }
